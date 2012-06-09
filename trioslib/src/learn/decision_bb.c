2009c965f95f678458040b71005af66f63e9edff
Igor Montagner <igordsm@gmail.com>
2012-06-09 16:49:36 -0300

    Doxygen and renaming of decision functions.

--- trioslib/src/learn/decision_bb.c
+++ trioslib/src/learn/decision_bb.c
@@ -23,174 +23,19 @@
 /*       frequency of that example)                                         */
 
 
+/*!
+    Makes the decision for BB and BG. Recursive version.
 
-/*
-     -------------------------------------------
-     FUNCTION: MTM_DECIDE
-     -------------------------------------------
+    \param p Set of examples.
+    \param mtm Classified examples set.
+    \param sum Total number of examples.
+    \param wzip Number of bytes of the w-pattern.
+    \param freqmin Minimal frequency of a w-pattern. If the pattern has a smaller frequency it is ignored.
+    \param estimator Decision estimator.
+    \param undflabel Value assigned to the undecidable examples.
+    \return 1 on success. 0 on failure.
 */
-
-mtm_t               /*+ Purpose: Builds the mtm table from the examples    +*/
- *mtm_decide(
-   xpl_t *xpl,      /*+ In: pointer to examples                            +*/
-   int   comp_prob, /*+ In: A flag indicating if the probabiities should be
-                            computed (0=NO, 1=YES)                         +*/
-   double freqmin,   /*+ In: Minimun frequency of an example to be accepted +*/
-   double estimator, /*+ In: Decision Estimator                             +*/
-   int   comptype,  /*+ In: type of computation for the undecidable cases  +*/
-   int   undflabel  /*+ In: Value to be assigned to the undecidable
-                            examples, if comptype==1                       +*/
-)
-/*+ Return: Pointer to mtm                                                 +*/
-{
-/*  author: Nina S. Tomita, R. Hirata Jr. (nina@ime.usp.br)                 */
-/*  date: Tue Oct 29 1996                                                   */
-
-/*  Modification by:  Nina S. Tomita, R. Hirata Jr. (nina@ime.usp.br)       */
-/*  Date: Tue Jan  6 1998                                                   */
-/*  Mod: Added a new parameter - comp_prob                                  */
-/*  Mod: Routine modified to compute the probabilities P(x) and p(y|x) if   */
-/*       comp_prob== 1                                                      */
-
-/*  Modification by:  Nina S. Tomita, R. Hirata Jr. (nina@ime.usp.br)       */
-/*  Date: Sun Feb 20 2000                                                   */
-/*  Mod: treatment of the undecidable cases (tie condition) fixed and       */
-/*       improved. New parameter comptype introduced                        */
-
-  mtm_t *mtm ;
-  int   type, wsize ;
-  unsigned int nmtm, sum ;
-
-
-  wsize = xpl_get_wsize( xpl ) ;
-
-  type  = xpl_get_type( xpl ) ;
-
-  if((type!=BB) && (comp_prob)) {
-    return (mtm_t *)trios_error(1,
-      "Probabilities can be computed only for binary to binay operators.\
-       Other cases not implemented yet.") ;
-  }
-
-  sum = xpl_get_sum ( xpl ) ;
-  
-  /* The number of examples in the classified examples set may be 
-     smaller than in the input examples set because some examples
-     may be ignored due to their small frequency. However, we decided
-     that the classified examples set will have the same number of nodes
-     (i.e., rows) of the examples set because their size will be very
-     close after the decision. The previous version of this routine
-     used linked lists to save memory, but we noticed that it was
-     time consuming and it's preferable to have a memory consuming
-     routine (but not too excessively, of course) than time consuming. */
-
-  nmtm  = xpl_get_n_nodes( xpl ) ;
-
-#ifdef _DEBUG_1_
-  pac_debug("nmtm=%d, type=%d", nmtm, xpl->type) ;
-#endif
-
-  if (NULL == (mtm = (mtm_t *)mtm_create( wsize, type, nmtm ))) {
-    return(mtm_t *)trios_error(MSG, "mtm_decide: mtm_create failure") ;
-  } 
-
-#ifdef _DEBUG_
-  pac_debug("mtm_create() done. wsize=%d, type=%d, nmtm=%d", wsize, type, nmtm) ;
-#endif
-  
-
-  /* Now we've to reset the value of nmtm because it will be different 
-     depending on the decisions                                           */
-
-  mtm_set_nmtm( mtm, 0 );
-  
-  switch ( type ) {
-
-    case BB: {
-
-      if (undflabel > 1) {
-        undflabel = 1 ; /* Nonsense to put a label > 1 for BB */
-        trios_warning("Label for the undecidable examples will be 1.") ;
-      }
-
-      /* For binary cases, the estimators media, moda and median will be set
-	 to be the same                                                     */
-
-      if ( (estimator != MABS) && ((estimator > 1) || (estimator < 0.5)) ) {
-	estimator = (float) AVERAGE ; 
-      }
-
-#ifdef _DEBUG_
-      pac_debug("BIN mtmdecide estimador = %f \n", estimator) ;
-#endif
-
-      /* This is a call for the RECURSIVE VERSION
-      if(!decide_tree_bb((xpl_BB *)xpl->root, mtm, sum, xpl->wzip,
-                          freqmin, estimator, undflabel, comp_prob)) {
-	xpl_free( xpl ) ;
-        mtm_free( mtm ) ;
-        return(mtm_t *)pac_error(MSG, "mtm_decide: decide examples failure") ;
-      } 
-      */
-
-      if(!decide_ll_bb( xpl, mtm, freqmin, estimator, undflabel, comp_prob )) {
-	mtm_free( mtm ) ;
-	return(mtm_t *)trios_error(MSG, "mtm_decide: decide examples failure") ;
-      } 
-
-      mtm_set_comp_prob( mtm, comp_prob ) ;
-
-      break ;
-    }   
-    
-    case BG: {
-        return (mtm_t *) trios_error(1, "BG operators not supported yet");
-        break ;
-    }   
-
-    case GG:
-    case WKC:
-    case WKF:
-    case WK3F:
-    case WK3C:
-    case WKGG2F:
-    case WKGG2C:
-    case GG3: {
-        mtm_free( mtm ) ;
-        return (mtm_t *) trios_error(1, "Gray-level operators not supported yet");
-        break ;
-    }   
-    
-    default: {  
-        mtm_free( mtm ) ;
-        return (mtm_t *)trios_error(1,
-			       "mtm_decide: type GB not implemented yet.") ;
-        break ; 
-    } 
-  }
-  return(mtm);
-}
-
-
-
-/*
-     ------------------------------------------------------------
-     FUNCTION: DECIDE_TREE_BB - Recursive version of the decision
-     ------------------------------------------------------------
-*/
-
-int                  /*+ Purpose: Makes the decision  for BB and BG         +*/
-  decide_tree_bb(
-    xpl_BB *p,       /*+ In: Begining  of the examples                      +*/
-    mtm_t *mtm,      /*+ In/Out:  Pointer to minterms                       +*/
-    unsigned int sum,/*+ In: Total number of examples                       +*/
-    int   wzip,      /*+ In: Number of bytes to put wpatern                 +*/
-    double freqmin,   /*+ In: Minimal frequency to consider an example, in % +*/
-    double estimator, /*+ In: Decision Estimator                             +*/
-    int   undflabel  /*+ In: Value assigned to the undecidable examples     +*/
-)
-/*+ Return: 1 on Success 0 on Failure                                       +*/ 
-{
+int decide_tree_bb(xpl_BB *p, mtm_t *mtm, unsigned int sum, int wzip, double freqmin, double estimator, int undflabel) {
 /*  author: Nina S. Tomita, R. Hirata Jr. (nina@ime.usp.br)                  */
 /*  date: Wed Oct 30 1996                                                    */
   
@@ -297,23 +142,17 @@ pac_debug("mtmbbtree Vai inserir wpat[0]=%d label=%d", p->wpat[0], wpat_label) ;
 }
 
 
-/*
-     -------------------------------------------
-     FUNCTION: decide_ll_bb
-     -------------------------------------------
+/*!
+    Makes the decision for BB and BG. Iterative version.
+
+    \param xpl Set of examples.
+    \param mtm Classified examples set.
+    \param freqmin Minimal frequency of a w-pattern. If the pattern has a smaller frequency it is ignored.
+    \param estimator Decision estimator.
+    \param undflabel Value assigned to the undecidable examples.
+    \return 1 on success. 0 on failure.
 */
- 
-int                  /*+ Purpose: Makes the decision for BB case            +*/
-  decide_ll_bb(
-    xpl_t *xpl,      /*+ In: set of examples                                +*/
-    mtm_t *mtm,      /*+ In/Out:  Pointer to minterms                       +*/
-    double freqmin,   /*+ In: Minimal frequency to consider an example, in % +*/
-    double estimator, /*+ In: Decision Estimator                             +*/
-    int   undflabel, /*+ In: Value assigned to the undecidable examples     +*/
-    int   comp_prob  /*+ In: flag indicating if prob. should be computed    +*/
-)
-/*+ Return: 1 on Success 0 on Failure                                       +*/ 
-{ 
+int decide_ll_bb(xpl_t *xpl, mtm_t *mtm, double freqmin, double estimator, int undflabel, int comp_prob) {
 /*  author: Nina S. Tomita, R. Hirata Jr. (nina@ime.usp.br)                 */
 /*  date: Tue Jan  6 1998                                                   */
 
