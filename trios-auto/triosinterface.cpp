312b7e1c2b1348b888c27fef48f295c14c74a6fb
Igor Montagner <igordsm@gmail.com>
2012-10-18 14:53:49 -0300

    Fix compile error in trios-auto

--- trios-auto/triosinterface.cpp
+++ trios-auto/triosinterface.cpp
@@ -17,7 +17,7 @@ QString TriosInterface::apply(QString input_path, QString operator_path) {
     QString result_path = "result.pgm";
     QDir dir;
     dir.remove("result.pgm");
-    int r = lpapplic((char *)input_path.toStdString().c_str(), (char *)operator_path.toStdString().c_str(), NULL, 0, 0, 255, (char *)result_path.toStdString().c_str());
+    int r = lpapplic_disk((char *)input_path.toStdString().c_str(), (char *)operator_path.toStdString().c_str(), NULL, 0, 0, 255, (char *)result_path.toStdString().c_str());
     if (r == 1) {
         return dir.absoluteFilePath(result_path);
     } else {
@@ -29,7 +29,7 @@ void build_run(char *w_path, char *s_path, TriosInterface *trios) {
     if (!lcollec(s_path, w_path, NULL, 1, 1, 0, "result.xpl", NULL)) {
         trios->send_build_finished("");
     }
-    if (!ldecision("result.xpl",  1, 0, AVERAGE, 0, 0, "decision.mtm")) {
+    if (!ldecision_disk("result.xpl",  1, 0, AVERAGE, 0, 0, "decision.mtm")) {
         trios->send_build_finished("");
     }
     window_t *w = win_read(w_path);
@@ -37,7 +37,7 @@ void build_run(char *w_path, char *s_path, TriosInterface *trios) {
     itv_write("itv_temp.itv", i, w);
     win_free(w);
     itv_free(i);
-    if (!lisi("decision.mtm", "itv_temp.itv", 3, 5, 0, 0, "itv_final.itv", 0, NULL, NULL)) {
+    if (!lisi_disk("decision.mtm", "itv_temp.itv", 3, 5, 0, 0, "itv_final.itv", 0, NULL, NULL)) {
         trios->send_build_finished("");
     }
     trios->send_build_finished("itv_final.itv");
