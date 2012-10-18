ab8f4bf0d9bf3f764d671a94fd4d66ae1b0e3570
Igor Montagner <igordsm@gmail.com>
2012-10-18 15:02:25 -0300

    d

--- trioslib/src/opencv_interface/opencv_interface.h
+++ trioslib/src/opencv_interface/opencv_interface.h
@@ -2,12 +2,12 @@
 #include "trios_img.h"
 #include "trios_win.h"
 #include "trios_apert.h"
+#include "trios_learn.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef void dTree;
 
 dTree *train_cv_tree(mtm_t *mtm);
 img_t *apply_cv_tree(img_t *input, dTree *tree, window_t *win, img_t *mask);
