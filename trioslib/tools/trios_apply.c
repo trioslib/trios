b3680ba1f3c33d268569e38544cf94c839affe7e
igor <igordsm@gmail.com>
2012-11-26 21:35:07 -0200

    Add itv_t support for trios_apply

--- trioslib/tools/trios_apply.c
+++ trioslib/tools/trios_apply.c
@@ -3,21 +3,28 @@
 int main(int argc, char *argv[]) {
     image_operator_t *op = NULL;
     multi_level_operator_t *mop = NULL;
-    img_t *input, *output;
+    itv_t *old_operator = NULL;
+    window_t *old_window = NULL;
+    img_t *input, *output, *mask = NULL;
 
     if (argc < 4) {
         printf("TRIOS tools: apply\n");
-        printf("Usage: $trios-apply path_to_operator path_to_image path_to_result\n");
+        printf("Usage: $trios-apply path_to_operator path_to_image path_to_result optional_mask\n");
         return -1;
     }
 
     op = image_operator_read(argv[1]);
     if (op == NULL) {
-        printf("Error reading operator Trying to read as multi-level operator.\n");
-        mop = multi_level_operator_read(argv[1]);
-        if (mop == NULL) {
-            printf("Invalid operator!\n");
-            return -1;
+
+        printf("Trying to read as itv...\n");
+        old_operator = itv_read(argv[1], &old_window);
+        if (old_operator == NULL) {
+            printf("Trying to read as multi-level operator...\n");
+            mop = multi_level_operator_read(argv[1]);
+            if (mop == NULL) {
+                printf("Invalid operator!\n");
+                return -1;
+            }
         }
     }
 
@@ -27,10 +34,16 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    if (argc == 5) {
+        mask = img_readPGM(argv[4]);
+    }
+
     if (op != NULL) {
-        output = image_operator_apply(op, input, NULL);
+        output = image_operator_apply(op, input, mask);
+    } else if (mop != NULL){
+        output = multi_level_apply(mop, input, mask);
     } else {
-        output = multi_level_apply(mop, input, NULL);
+        output = lpapplic_memory(input, old_operator, old_window, mask, 0, 0, 255);
     }
 
     if (output == NULL) {
