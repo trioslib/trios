c8a9c7b768cd866a80af39709ebbcc715d4916db
Igor Montagner <igordsm@gmail.com>
2013-06-06 12:04:00 -0300

    Add doxygen to image_operator_t.

--- trioslib/include/trios_operator.h
+++ trioslib/include/trios_operator.h
@@ -43,18 +43,78 @@ typedef struct {
  */
 image_operator_t *image_operator_itv(itv_t *itv, window_t *win);
 
+/*!
+ * \brief Builds a BB image operator.
+ * \param set Training set.
+ * \param win Window.
+ * \return Trained BB image operator.
+ */
 image_operator_t *image_operator_build_bb(imgset_t *set, window_t *win);
+
+/*!
+ * \brief Builds a GG image operator.
+ * \param set Training set.
+ * \param win Window.
+ * \return Trained GG image operator.
+ */
 image_operator_t *image_operator_build_gg(imgset_t *set, window_t *win);
+
+/*!
+ * \brief Builds a GB image operator. The decision process uses the MEDIAN criteria.
+ * \param set Training set.
+ * \param win Window.
+ * \return Trained GB image operator.
+ */
 image_operator_t *image_operator_build_gb(imgset_t *set, window_t *win);
 
+/*!
+ * \brief Builds a WKC image operator. This operation is not implemented yet.
+ * \param set Training set.
+ * \param win Window.
+ * \param apt Aperture configuration.
+ * \return Trained WKC image operator.
+ */
 image_operator_t *image_operator_build_wkc(imgset_t *set, window_t *win, apert_t *apt);
+
+/*!
+ * \brief Builds a WKF image operator. This operation is not implemented yet.
+ * \param set Training set.
+ * \param win Window.
+ * \param apt Aperture configuration.
+ * \return Trained WKF image operator.
+ */
 image_operator_t *image_operator_build_wkf(imgset_t *set, window_t *win, apert_t *apt);
 
+/*!
+ * \brief Transforms an image using an image operator.
+ * \param iop Image operator.
+ * \param input Input image.
+ * \param mask Application mask.
+ * \return
+ */
 img_t *image_operator_apply(image_operator_t *iop, img_t *input, img_t *mask);
 
+/*!
+ * \brief Calculates the MAE of an image operator in a test set.
+ * \param iop Image operator.
+ * \param test Test set.
+ * \param acc Accuracy.
+ * \return The MAE of the image operator.
+ */
 unsigned long image_operator_mae(image_operator_t *iop, imgset_t *test, double *acc);
+
+/*!
+ * \brief Calculates the MSE of an image operator in a test set.
+ * \param iop Image operator.
+ * \param test Test set.
+ * \return The MSE of the image operator.
+ */
 unsigned long image_operator_mse(image_operator_t *iop, imgset_t *test);
 
+/*!
+ * \brief Frees the memory used by an image operator.
+ * \param iop Image operator.
+ */
 void image_operator_free(image_operator_t *iop);
 
 #ifdef __cplusplus
