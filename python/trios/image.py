9992491508d26165737df6a265883c14b359356f
Igor Montagner <igordsm@gmail.com>
2013-06-14 09:49:05 -0300

    Will use PIL for the image structure.
    Window now a subclass of ndarray.

--- python/trios/image.py
+++ python/trios/image.py
@@ -5,16 +5,4 @@ to read/write images to/from the disk.
 
 import numpy as np
 
-
-class Image(np.ndarray):
-    pass
-    
-    def write(self, fname):
-        pass
-    
-    def show(self):
-        pass
-
-def read(fname):
-    return Image(shape=(2, 2), dtype='int32')
-
+from Image import Image, open
--- python/trios/window.py
+++ python/trios/window.py
@@ -9,6 +9,52 @@ import matplotlib.lines
 import matplotlib.pyplot as plt
 
 
+class Window(np.ndarray):
+	
+	def __new__(cls, h, w):
+		obj = np.ndarray.__new__(cls, shape=(h, w), dtype=bool)
+		return obj
+		
+	def write(self, name):
+		f = open(name, 'w')
+		f.write("""WINSPEC ########################################################
+	.h %d
+	.w %d
+	.d
+	""" % self.shape)
+		h, w = self.shape
+		for i in range(h):
+			for j in range(w):
+				if self[i, j]:
+					f.write('1 ')
+				else:
+					f.write('0 ')
+			f.write('\n')
+		f.close()
+		
+	def show(self):
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+		h, w = self.shape
+		plt.xlim(0, w)
+		plt.ylim(0, h)
+		
+		for i in range(h):
+			for j in range(w):
+				if self[i, j]:
+					p = matplotlib.patches.Circle( (j + 0.5, h - i - 0.5), 0.35, color='black')
+					ax.add_patch(p)
+		
+		for i in range(h):
+			l = matplotlib.lines.Line2D([0, w], [i, i], color='black')
+			ax.add_line(l)
+			
+		for j in range(w):
+			l = matplotlib.lines.Line2D([j, j], [0, h], color='black')
+			ax.add_line(l)
+		plt.show()
+
+
 def read(f_name):
     f = open(f_name)
     s = f.read()
@@ -19,9 +65,7 @@ def read(f_name):
     h = int(lines[1][3:])
     w = int(lines[2][3:])
     # le janela
-    win = []
-    
-    win = np.zeros((h, w), dtype=bool)
+    win = Window(h, w)
     
     for i in range(h):
         l = lines[i+4].split(' ')[:w]
@@ -29,42 +73,3 @@ def read(f_name):
             if l[j] != '0':
                 win[i, j] = True
     return win
-
-def write(name, win):
-    f = open(name, 'w')
-    f.write("""WINSPEC ########################################################
-.h %d
-.w %d
-.d
-""" % win.shape)
-    h, w = win.shape
-    for i in range(h):
-        for j in range(w):
-            if win[i, j]:
-                f.write('1 ')
-            else:
-                f.write('0 ')
-        f.write('\n')
-    f.close()
-
-def show(win):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    h, w = win.shape
-    plt.xlim(0, w)
-    plt.ylim(0, h)
-    
-    for i in range(h):
-        for j in range(w):
-            if win[i, j]:
-                p = matplotlib.patches.Circle( (j + 0.5, h - i - 0.5), 0.35, color='black')
-                ax.add_patch(p)
-    
-    for i in range(h):
-        l = matplotlib.lines.Line2D([0, w], [i, i], color='black')
-        ax.add_line(l)
-        
-    for j in range(w):
-        l = matplotlib.lines.Line2D([j, j], [0, h], color='black')
-        ax.add_line(l)
-    plt.show()
