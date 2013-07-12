e70b535bd138330ebc925cc01a7e55eed13cae1c
igor <igordsm@gmail.com>
2013-07-12 20:13:52 -0300

    Add code to find the path of trios.

--- python/trios/detect.py
+++ python/trios/detect.py
@@ -3,8 +3,20 @@ import os.path
 
 TRIOS_PATH = ''
 
-def detect_trios():
-	pass
+
+
+def detect_trios(pth=''):
+    if os.system('trios_build') >= 0:
+        TRIOS_PATH = ''
+    elif os.system('./trios_build') >= 0:
+        TRIOS_PATH = os.getcwd()
+    elif os.system(os.path.join(pth, 'trios_build')) >= 0:
+        TRIOS_PATH = pth
+    else:
+        raise OSError(-1, 'TRIOS not found.')
+        
+    
+
 
 def call(cmd):
     print(os.path.join(TRIOS_PATH, cmd))
