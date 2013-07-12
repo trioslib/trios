a76903348807d8d13e772e7f840c4e5a18f529dd
Igor Montagner <igordsm@gmail.com>
2013-07-12 15:12:17 -0300

    Add docs

--- python/trios/detect.py
+++ python/trios/detect.py
@@ -1,12 +1,22 @@
+"""
+Detects TRIOS command line tools. The path is stored in TRIOS_PATH.
+"""
+
 import os
 import os.path
 
 TRIOS_PATH = ''
 
 def detect_trios():
-	pass
+    """
+    Looks for TRIOS command line tools in some paths.
+    """
+    pass
 
 def call(cmd):
+    """
+    Calls a command line tool from TRIOS.
+    """
     print(os.path.join(TRIOS_PATH, cmd))
     return os.system(os.path.join(TRIOS_PATH, cmd))
 
--- python/trios/image.py
+++ python/trios/image.py
@@ -1,8 +1,5 @@
 """
-TRIOS uses numpy 2D arrays as images. The read_image/write_image are used
-to read/write images to/from the disk.
+Just a proxy for PIL Image.
 """
-
-import numpy as np
 
 from Image import Image, open
