bed8656479b0b13e076398d4b429f52035a3630a
Igor Montagner <igordsm@gmail.com>
2013-12-08 11:34:35 -0200

    Read scores file

--- trioslib/tools/trios_xpl_util.c
+++ trioslib/tools/trios_xpl_util.c
@@ -2,7 +2,7 @@
 #include "string.h"
 #include "ctype.h"
 
-void resample_xpl(xpl_t *domain, window_t *win_domain, window_t *win, char *dest) {
+int resample_xpl(xpl_t *domain, window_t *win_domain, window_t *win, char *dest) {
     xpl_t *new_xpl = NULL;
     int n_remove, *to_remove;
     int i, j, k, w, h;
@@ -28,9 +28,33 @@ void resample_xpl(xpl_t *domain, window_t *win_domain, window_t *win, char *dest
     xpl_write(dest, new_xpl, win, NULL);
 }
 
+double *read_scores(window_t *win_domain, char *score_file) {
+    double *scores = NULL;
+    double s;
+    FILE *f;
+    int k, i, j, w, h, n;
+
+
+    n = win_get_wsize(win_domain);
+    scores = malloc(sizeof(double) * n);
+
+    f = fopen(score_file, "r");
+
+    for(k = 0; k < n; k++) {
+        scanf("%d %d %lf", &i, &j, &s);
+        printf("Scores: %d %d %f\n", i, j, s);
+    }
+
+}
+
+void fill_xpl(xpl_t *domain, window_t *win_domain, double *scores, char *dest) {
+
+}
+
 int main(int argc, char *argv[]) {
     window_t *win, *win_domain;
-    xpl_t *domain, *new_xpl;
+    xpl_t *domain;
+    double *scores;
 
 
     if (strcmp(argv[1], "resample_xpl") == 0) {
@@ -38,7 +62,10 @@ int main(int argc, char *argv[]) {
         win = win_read(argv[3]);
         resample_xpl(domain, win_domain, win, argv[4]);
     } else if (strcmp(argv[1], "fill_xpl") == 0) {
-
+        /* usage: fill_xpl xpl variable_score */
+        domain = xpl_read(argv[2], &win_domain, NULL);
+        scores = read_scores(win_domain, argv[3]);
+        fill_xpl(domain, win_domain, scores, argv[4]);
     }
 
     return 0;
