96bf3945dd6a74944143bb477cf5fc604d69bcde
Igor Montagner <igordsm@gmail.com>
2013-12-08 11:23:26 -0200

    Change trios_xpl_util to accept other command lines.^

--- trioslib/tools/trios_xpl_util.c
+++ trioslib/tools/trios_xpl_util.c
@@ -2,14 +2,11 @@
 #include "string.h"
 #include "ctype.h"
 
-int main(int argc, char *argv[]) {
-    window_t *win_domain, *win;
-    xpl_t *domain, *new_xpl;
+void resample_xpl(xpl_t *domain, window_t *win_domain, window_t *win, char *dest) {
+    xpl_t *new_xpl = NULL;
     int n_remove, *to_remove;
     int i, j, k, w, h;
 
-    domain = xpl_read(argv[1], &win_domain, NULL);
-    win = win_read(argv[2]);
     n_remove = win_get_wsize(win_domain) - win_get_wsize(win);
     trios_malloc(to_remove, sizeof(int) * n_remove, int, "Failed to alloc aux array.");
     w = win_get_width(win_domain);
@@ -26,9 +23,23 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-    printf("n remove %d k %d\n", n_remove, k);
-    new_xpl = xpl_remove_variables(domain, to_remove, n_remove);
 
-    xpl_write(argv[3], new_xpl, win, NULL);
+    new_xpl = xpl_remove_variables(domain, to_remove, n_remove);
+    xpl_write(dest, new_xpl, win, NULL);
+}
+
+int main(int argc, char *argv[]) {
+    window_t *win, *win_domain;
+    xpl_t *domain, *new_xpl;
+
+
+    if (strcmp(argv[1], "resample_xpl") == 0) {
+        domain = xpl_read(argv[2], &win_domain, NULL);
+        win = win_read(argv[3]);
+        resample_xpl(domain, win_domain, win, argv[4]);
+    } else if (strcmp(argv[1], "fill_xpl") == 0) {
+
+    }
+
     return 0;
 }
