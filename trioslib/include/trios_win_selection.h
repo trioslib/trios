6b25fb3acb16945309d59df4bda53c3cb49e69e2
Igor Montagner <igordsm@gmail.com>
2014-02-07 17:58:35 -0200

    Definition of an API for window selection.

---
+++ trioslib/include/trios_win_selection.h
@@ -0,0 +1,40 @@
+#ifndef TRIOS_WIN_SELECTION_H
+#define TRIOS_WIN_SELECTION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "trios_win.h"
+#include "trios_xpl.h"
+#include "trios_mtm.h"
+
+
+
+/* other methods for window selection will be implemented! */
+
+/*!
+ * Runs the Relief algorithm to select a subwindow of domain with n points.
+ * \param xpl Set of patterns extracted from an imageset.
+ * \param domain A window containing all the points to be considered. 
+ * \param n The number of points in the desired window.
+ * \return A window with n points.
+ * \sa lcollec_memory
+ */
+window_t *window_relief(xpl_t *xpl, window_t *domain, int n);
+
+/*!
+ * Resample the xpl_domain patterns using new_win. Useful when the lcollec_* functions take a long time or when
+ * determining two-level architectures.
+ * \param xpl_domain Patterns extracted with win_domain.
+ * \param win_domain A (tipically big) window.
+ * \param new_win A subwindow of win_domain.
+ * \return The xpl_domain patterns as if they were observed using new_win.
+ */
+xpl_t *window_resample_xpl(xpl_t *xpl_domain, window_t *win_domain, window_t *new_win);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif // TRIOS_WIN_SELECTION_H
