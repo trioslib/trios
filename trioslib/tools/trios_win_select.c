e1a6cbd553184ebf81efe659a3457689fa3ee1d9
Igor Montagner <igordsm@gmail.com>
2014-05-13 16:48:20 -0300

    Add a call to two_level_relief in trios_win_select.

--- trioslib/tools/trios_win_select.c
+++ trioslib/tools/trios_win_select.c
@@ -6,14 +6,17 @@
 void print_usage() {
     printf("Usage:\n"
            "\t trios_win_select relief xpl_file window_size number_of_prototypes result_path (optional)seed\n"
-           "\t trios_win_select martins_barrera xpl_file domain result_path\n");
+           "\t trios_win_select martins_barrera xpl_file domain result_path\n"
+           "\t trios_win_select two_level_relief imgset1 imgset2 n1stlevel win_size nproto1 nproto2 \n");
 }
 
 int main(int argc, char *argv[]) {
     window_t *new_window;
     xpl_t *xpl;
     window_t *domain;
+    imgset_t *T1, *T2;
     int num_proto, win_size, seed;
+    int num_proto2, n1stlevel;
     char *dest;
     
     if (argc < 4) {
@@ -43,6 +46,19 @@ int main(int argc, char *argv[]) {
         }
         new_window = window_martins_barrera(xpl, domain, NULL);
         win_write(argv[4], new_window);
+    } else if (strcmp(argv[1], "two_level_relief") == 0) {
+        if (argc < 8) {
+            print_usage();
+            return -1;
+        }
+        T1 = imgset_read(argv[2]);
+        T2 = imgset_read(argv[3]);
+        n1stlevel = atoi(argv[4]);
+        win_size = atoi(argv[5]);
+        num_proto = atoi(argv[6]);
+        num_proto2 = atoi(argv[7]);
+        
+        window_two_level_relief(T1, T2, n1stlevel, win_size, num_proto, num_proto2);
     }
 
     /* writes selected window */
