ea5fbe5d0cb32301d0dd8ff5b212f89dfa69c7a2
Igor dos Santos Montagner (PhD | hirata) <igordsm@gmail.com>
2014-10-09 15:37:24 -0300

    Version 1 one the black box ICPR method.

--- trioslib/src/window_selection/two_level_relief_icpr14.c
+++ trioslib/src/window_selection/two_level_relief_icpr14.c
@@ -79,8 +79,9 @@ void compare_xpl(xpl_t *xpl1, xpl_t *xpl2) {
 }
 
 int select_number_of_first_level_operators(xpl_t *xpl2nd, point_weight *ranking, int n_windows, xpl_t **level2_new) {
-    int i, *to_remove, *to_remove_iter;
+    int i, j, *to_remove, *to_remove_iter;
     xpl_t *temp = NULL;
+    window_t *w = NULL;
     
     trios_malloc(to_remove, sizeof(int) * n_windows, int, "Failed to alloc array");
     for (i = 0; i < n_windows; i++) {
@@ -97,11 +98,17 @@ int select_number_of_first_level_operators(xpl_t *xpl2nd, point_weight *ranking,
     } while (i < n_windows && temp->n_nodes != ( 1 << (n_windows - i) ) );
     *level2_new = temp;
     
+    w = win_create(1, n_windows - i, 1);
+    for (j = 0; j < n_windows - i; j++) {
+        win_set_point(0, j, 1, 1, w);
+    }
+    xpl_write("xpl2ndnew", temp, w, NULL);
+    
     return n_windows - i;
 }
 
 multi_level_operator_t *window_two_level_relief(window_t *domain, imgset_t *T1, imgset_t *T2, int n_1st_level, int sz_1st_level, int n_proto1, int n_proto2, int type) {
-    image_operator_t **ops, *temp_op;
+    image_operator_t **ops, **ordered_ops;
     multi_level_operator_t *result;
     window_t **wins;
     window_t *temp, *temp2;
@@ -112,15 +119,19 @@ multi_level_operator_t *window_two_level_relief(window_t *domain, imgset_t *T1, 
     
     trios_malloc(ops, sizeof(image_operator_t *) * n_1st_level, window_t **, "Failed to alloc operator vector\n");
     trios_malloc(wins, sizeof(window_t) * n_1st_level, window_t **, "Failed to alloc window vector\n");
+    fprintf(stderr, "Collec started...\n");
     xpl_domain = lcollec_memory(T1, domain, type);
+    fprintf(stderr, "Collec finished...\n");
     
     for (i = 0; i < n_1st_level; i++) {
+        fprintf(stderr, "Relief: %d, seed: %d\n", i, 100*i + i);
         wins[i] = window_relief(xpl_domain, domain, n_proto1, sz_1st_level, NULL, i*100 + i);
         sprintf(cmd, "win-%d", i);
         win_write(cmd, wins[i]);
     }
     
     for (i = 0; i < n_1st_level; i++) {
+        fprintf(stderr, "Training operator %d\n", i);
         if (type == BB) {
             ops[i] = image_operator_build_bb(T1, wins[i]);
         } else if (type == GB) {
@@ -134,19 +145,21 @@ multi_level_operator_t *window_two_level_relief(window_t *domain, imgset_t *T1, 
         image_operator_write(cmd, ops[i]);
     }
     
+    fprintf(stderr, "Collec for 2nd level started...\n");
     level2 = lcollec_two_level(T2, ops, n_1st_level);
+    fprintf(stderr, "Collec for 2nd level finished...\n");
     
     temp = win_create(1, n_1st_level, 1);
     for (i = 0; i < n_1st_level; i++) {
-        win_set_point(1, i, 1, 1, temp);
+        win_set_point(0, i, 1, 1, temp);
     }
     
     xpl_write("xpl2ndlevel", level2, temp, NULL);
     
+    fprintf(stderr, "Relief for 2nd level started...\n");
     temp2 = window_relief(level2, temp, n_proto2, 1, &operator_ranking, 0);
-    
+    fprintf(stderr, "Selecting number of first level operators...\n");
     num1stlevel = select_number_of_first_level_operators(level2, operator_ranking, n_1st_level, &level2_new);
-    
     /* faz lista dos operadores de primeiro nível
      * manda para o multi_level_combine_xpl
      * devolve o operador dois níveis treinado.
@@ -154,19 +167,17 @@ multi_level_operator_t *window_two_level_relief(window_t *domain, imgset_t *T1, 
      * esperamos que ele seja igual ao treinado usando as etapas individuais :) */
     
     /* put all selected operators in the first num1stlevel positions of ops */
+    trios_malloc(ordered_ops, num1stlevel * sizeof(image_operator_t*), multi_level_operator_t*, "Failed to alloc array.");
     for (i = 0; i < num1stlevel; i++) {
-        temp_op = ops[i];
-        ops[i] = ops[ operator_ranking[i].j ];
-        ops[ operator_ranking[i].j ] = temp_op;
+        ordered_ops[i] = ops[operator_ranking[i].j];
     }
+    result = multi_level_combine_xpl(ordered_ops, num1stlevel, level2_new);
+    
     for (i = num1stlevel; i < n_1st_level; i++) {
-        image_operator_free(ops[i]);
+        image_operator_free(ops[operator_ranking[i].j]);
     }
-    
-    result = multi_level_combine_xpl(level2_new, ops, num1stlevel);
-    
-    
     free(ops);
+    free(ordered_ops);
     free(wins);
     win_free(temp);
     win_free(temp2);
