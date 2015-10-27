c851538b3eb84b6568487519acc26052cd75ae0a
Igor Montagner <igordsm@gmail.com>
2015-10-27 15:36:43 +0100

    Add multiprocessing support for the CombinationPattern.

--- trios/feature_extractors/combination.pyx
+++ trios/feature_extractors/combination.pyx
@@ -5,6 +5,17 @@ from trios.WOperator import WOperator
 from trios.WOperator cimport FeatureExtractor
 import math
 
+import multiprocessing as mp
+from multiprocessing import sharedctypes
+import ctypes
+import itertools
+
+from trios.wop_matrix_ops import process_one_image
+
+import trios
+
+cimport numpy as np
+
 @cython.boundscheck(False)
 @cython.nonecheck(False)
 cpdef extract_pattern(CombinationPattern self, unsigned char[:,:] img, int i, int j, unsigned int [:] pat):
@@ -24,14 +35,28 @@ cpdef extract_pattern(CombinationPattern self, unsigned char[:,:] img, int i, in
             if lbl != 0:
                 pat[sh] = pat[sh] | (1 << byt)
     
-    
-def rebuild_comb(wops, bit):
-    return CombinationPattern(*wops, bitpack=bit)
+def init_pool(inp_, msk_, outs_):
+    global inp_par, msk_par, out_images
+    inp_par = inp_
+    msk_par = msk_
+    out_images = outs_
 
+def apply_parallel(t):
+    op, i, h, w = t
+    
+    img = np.frombuffer(inp_par, dtype=np.uint8).reshape((h, w))
+    msk = np.frombuffer(msk_par, dtype=np.uint8).reshape((h, w))
+    out = np.frombuffer(out_images[i], dtype=np.uint8).reshape((h, w))
+    #print('before apply', op, op.window, op.classifier, img.shape, msk.shape)
+    out[:] = op.apply(img, msk)
+    #print('end_apply')
+    return i
+    
 cdef class CombinationPattern(FeatureExtractor):
     cdef public list wops
     cdef public bint bitpack
     cdef public list fvectors 
+    cdef public int nprocs
     
     def __init__(self,  *wops, **kwargs):
         FeatureExtractor.__init__(self, np.ones((len(wops), 1), np.uint8))
@@ -42,6 +67,7 @@ cdef class CombinationPattern(FeatureExtractor):
             
         self.wops = list(wops)
         self.fvectors = [wop.extractor.temp_feature_vector() for wop in wops]
+        self.nprocs = 1
 
     def __len__(self):
         if self.bitpack:
@@ -52,6 +78,60 @@ cdef class CombinationPattern(FeatureExtractor):
     
     def temp_feature_vector(self):
         return np.zeros(len(self), np.uint32)
+    
+    
+    @cython.boundscheck(False)
+    @cython.nonecheck(False)
+    cpdef extract_batch(self, unsigned char[:,:] inp, unsigned char[:,:] out, unsigned char[:,:] msk, np.ndarray _X, unsigned char[:] y, int k):
+        cdef unsigned int[:,:] X = _X
+        cdef unsigned char[:,:] img
+        cdef int i, j, l, sh, byt
+        cdef long imgsize = inp.shape[0] * inp.shape[1]
+        
+        if self.nprocs == 1:
+            return FeatureExtractor.extract_batch(self, inp, out, msk, _X, y, k)
+       
+        inp_shared = sharedctypes.RawArray(ctypes.c_ubyte, np.asarray(inp).flat)
+        msk_shared = sharedctypes.RawArray(ctypes.c_ubyte, np.asarray(msk).flat)
+        out_shared = [sharedctypes.RawArray(ctypes.c_ubyte, imgsize) for i in range(len(self.wops))]        
+        outnp = [np.frombuffer(out_shared[i], 
+                       dtype=np.uint8).reshape((inp.shape[0], inp.shape[1]))
+                        for i in range(len(self.wops))]
+        pool = mp.Pool(processes=self.nprocs, initializer=init_pool, 
+                       initargs=(inp_shared, msk_shared, out_shared))
+        args = list(zip(self.wops, range(len(self.wops)), 
+                        itertools.repeat(inp.shape[0]), 
+                        itertools.repeat(inp.shape[1])))
+        res = pool.map(apply_parallel, args)
+        pool.close()
+        pool.join()
+        del pool
+        
+        del inp_shared
+        del msk_shared 
+        
+        for i in range(inp.shape[0]):
+            for j in range(inp.shape[1]):
+                if msk[i, j] == 0: continue
+                
+                y[k] = out[i, j]
+                
+                for l in range(len(self.wops)):
+                    img = outnp[l]
+                    if self.bitpack == False:                        
+                        X[k, l] = img[i, j]
+                    else:
+                        sh = int(l / 32)
+                        byt = l % 32
+                        if img[i, j] != 0:
+                            X[k, sh] = X[k, sh] | (1 << byt)
+                k += 1
+        
+        for i in range(len(out_shared)):
+            del out_shared[0]
+            
+        return k
+    
     
     cpdef extract(self, unsigned char[:,:] img, int i, int j, pat):
         extract_pattern(self, img, i, j, pat)
@@ -73,6 +153,3 @@ cdef class CombinationPattern(FeatureExtractor):
             self.wops.append(op)
         self.fvectors = [wop.extractor.temp_feature_vector() for wop in self.wops]
     
-    def __reduce__(self):
-        return (rebuild_comb, (self.wops, self.bitpack))            
-
