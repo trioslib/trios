8927e066897172f4f03383bccb82f70c0eb8171f
Igor Montagner <igordsm@gmail.com>
2016-01-08 15:47:05 +0100

    Add *binary* option to evaluate.compare_folders that return the rates of true positives, etc.

--- trios/shortcuts/evaluation.py
+++ trios/shortcuts/evaluation.py
@@ -4,6 +4,7 @@ Created on Fri Dec 11 10:11:40 2015
 
 @author: igor
 """
+from __future__ import print_function
 
 import trios
 import numpy as np
@@ -12,7 +13,21 @@ import scipy.ndimage
 import os
 import os.path
 
-from trios.wop_matrix_ops import compare_images
+from trios.wop_matrix_ops import compare_images, compare_images_binary
+
+import sys
+
+def __apply_parallel(op, elem):
+    pass
+
+def apply_and_save(operator, testset, folder_prefix, procs=None):
+    '''
+    Applies an operator in all images of a given testset. The
+    images are saved in folder_prefix with the same name as the
+    groundtruth images.
+    '''
+    
+    pass
 
 def compare_folders(testset, res_folder, binary=True):
     '''
@@ -22,17 +37,26 @@ def compare_folders(testset, res_folder, binary=True):
     the accuracy.
     '''
     
-    nerrs = 0
-    npixels = 0
+    if binary:
+        perf = np.zeros(5, np.uint32)
+    else:
+        perf = np.zeros(2, np.uint32)
     for (i, o, m) in testset:
-        print('Comparing', o)
+        print('Comparing', o, file=sys.stderr)
         out = sp.ndimage.imread(o, mode='L')
         o_name = os.path.split(o)[1]
         msk = sp.ndimage.imread(m, mode='L')
         res = sp.ndimage.imread('%s/%s'%(res_folder, o_name), mode='L')
         
-        n, npix = compare_images(out, msk, res, 0, 0)
-        nerrs += n
-        npixels += npix
+        if binary:
+            perf_i = compare_images_binary(out, msk, res, 0, 0)
+        else:
+            perf_i = compare_images(out, msk, res, 0, 0)
+        perf = perf + np.asarray(perf_i)
     
-    return nerrs/npixels
+    if binary:
+        return perf[:4]
+    else:
+        return perf[0] / perf[1]
+    
+    
