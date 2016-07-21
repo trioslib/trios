e03956985c5e2452ceacdea6c525fd56815be12b
Igor Montagner <igordsm@gmail.com>
2016-07-21 12:30:21 -0300

    Formatting changes in StaffHeuristic.

--- trios/contrib/staffs/staff_heuristic.py
+++ trios/contrib/staffs/staff_heuristic.py
@@ -4,57 +4,37 @@ from gamera.core import *
 from gamera.toolkits.musicstaves import musicstaves_rl_simple, musicstaves_rl_roach_tatem
 from gamera.toolkits.musicstaves.musicstaves import *
 from gamera.toolkits.musicstaves.musicstaves_skeleton import MusicStaves_skeleton
-from trios.feature_extractors import RAWFeatureExtractor, CombinationPattern
+from trios.feature_extractors import RAWFeatureExtractor
 
 import numpy as np
-import inspect 
-#from numpy to gamera
 from gamera.plugins import numpy_io
-
-#from gamera to numpy
-from gamera.plugins import numpy_io
-from numpy import fft
-
-# pode fazer essa classe usando python mesmo
 
 class StaffHeuristic(WOperator):
 
-    nomeMetodo = 'skeleton'
-    ms = None 
-    window = np.zeros((1,1),dtype=np.uint8)
-    extractor = RAWFeatureExtractor(window)
-    
     def __init__(self,nomeMetodo=None):
-        self.nomeMetodo = nomeMetodo
-
-        
-#usar os metodos do music staves para desenvolver 
+        self.nome_metodo = nomeMetodo
+        self.window = np.zeros((1, 1), dtype=np.uint8)
+        init_gamera()
 
     def apply(self, image_in_np, mask=None,batch=False):
-    #transformar imagem para o formato do music staves
-          
-        
-        init_gamera()
         image_gamera = numpy_io.from_numpy(image_in_np)
         image_gamera.to_onebit()
         image_gamera.invert()
 
-        if self.nomeMetodo == 'skeleton':
-             ms = MusicStaves_skeleton(image_gamera)
+        if self.nome_metodo == 'skeleton':
+            ms = MusicStaves_skeleton(image_gamera)
+        elif self.nome_metodo == 'rl_simple':
+            ms = musicstaves_rl_simple.MusicStaves_rl_simple(image_gamera)
+        elif self.nome_metodo == 'rl_roach':
+            ms = musicstaves_rl_roach_tatem.MusicStaves_rl_roach_tatem(image_gamera)
         else:
-            if self.nomeMetodo == 'rl_simple':
-                ms = musicstaves_rl_simple.MusicStaves_rl_simple(image_gamera)
-            else:
-                if self.nomeMetodo == 'rl_roach':
-                    ms = musicstaves_rl_roach_tatem.MusicStaves_rl_roach_tatem(image_gamera)
-    
+            raise Exception('Method not found: %s', self.nome_metodo)
+
         ms.remove_staves(crossing_symbols = 'bars')
         
         image_out =  ms.image.image_copy()
-    
         image_out_new =  image_out.to_numpy()
-         
-        ###pegar cada ponto do imageOut para o newArray 
+
         newArray = np.zeros(image_in_np.shape,dtype=np.uint8)
          
         height,width = image_in_np.shape
@@ -78,25 +58,21 @@ class StaffHeuristic(WOperator):
             cont = 0
             for i in range(height):
                 for j in range(width):             
-                     if mask[i,j]!=0:
-                           if image_out_new[i,j] != 0:
-                               batchArray[cont]=255
-                     cont = cont+1
+                    if mask[i,j]!=0:
+                        if image_out_new[i,j] != 0:
+                            batchArray[cont]=255
+                    cont = cont+1
 
         if batch : 
             return batchArray 
         else: 
             return newArray
     
-
-
-
     def train(self, imgset):
         pass
 
     def write_state(self, obj_dict):
-        obj_dict['nomeMetodo'] = self.nomeMetodo
+        obj_dict['nome_metodo'] = self.nome_metodo
         
     def set_state(self, obj_dict):
-        self.nomeMetodo = obj_dict['nomeMetodo']
-
+        self.nome_metodo = obj_dict['nome_metodo']
