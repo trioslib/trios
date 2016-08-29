e669b8262710cb12e33bd6514acf4520cca300b4
Igor <igordsm@gmail.com>
2016-08-29 18:07:03 -0500

    Script to run all examples.

---
+++ test/run_all.py
@@ -0,0 +1,13 @@
+import os
+
+if __name__ == '__main__':
+    for file in os.listdir(path='.'):
+        if file.endswith('test.py'):
+            print('Executando', file, end=' ')
+            with open(file) as script:
+                try:
+                    exec(script.read())
+                except Exception as e:
+                    print('FAIL!\nUm erro ocorreu em', file)
+                    print(e)
+        
