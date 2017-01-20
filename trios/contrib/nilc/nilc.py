a41486ed3466ba18eb990f09036a5a40aaf05213
Igor Montagner <igordsm@gmail.com>
2017-01-20 14:06:52 -0200

    partial version of nilc working

--- trios/contrib/nilc/nilc.py
+++ trios/contrib/nilc/nilc.py
@@ -4,7 +4,7 @@ Hirata, N. S.T., Hirata Jr., R., ....*.
 '''
 
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from trios.classifiers import SKClassifier
 import logging
 
@@ -17,40 +17,104 @@ def stop_criteria(j):
     '''
     return False
 
-def nilc_iteration(w, lamb, X_a, X_new, y):
+def nilc_iteration(X, y, w, nonzeros, k, lamb):
     '''
-    w: weight vector with
-    lamb: regularization parameter. C=1/lamb in sklearn
-    X_a: training set for non-zero weights
-    X_new: column-vector with responses from the new operator
+    X: second-level patterns. 
     y: labels
+    w: weight vector
+    nonzeros: selected operators    
+    lamb: regularization parameter. C=1/lamb in sklearn
     '''
     pass
     # compute optimality condition for new operator
     # if it is true, set w[j] = True and refit model
     # if not continue with w[j] = 0
 
-def nilc(training_set, operator_generator, domain, lamb=1, max_iter=100):
+def opt_condition(X_a, y, w, lamb, k):
+    lin = X_a.dot(w[:-1]) + w[-1]
+    exp = np.exp(lin)
+    p = exp/(1+exp)
+    opt = (1/lamb)*np.inner(X_a[:, k], y-p)
+    logger.info('Optimality condition: %f', opt)
+    return abs(opt) > 1
+    
+
+def nilc_precomputed():
+    max_iter = X_a.shape[1]
+    w = np.zeros(max_iter+1)
+    nonzeros_plus_bias = np.zeros(max_iter+1, np.bool)
+    nonzeros_plus_bias[-1] = True
+    nonzeros = nonzeros_plus_bias[:-1]
+    
+    model = LogisticRegression(penalty='l1', C=1/lamb, fit_intercept=True)
+
+    
+    for k in range(max_iter):
+        logger.info('Iteration %d', k)
+        
+        if k == 0:
+            nonzeros[0] = True
+            model.fit(X_a[:, nonzeros], y)
+            w[0] = model.coef_
+            w[-1] = model.intercept_
+        else:
+            if opt_condition(X_a, y, w, lamb, k):
+                nonzeros[k] = True
+                model.fit(X_a[:, nonzeros], y)
+                w[nonzeros_plus_bias] = model.coef_
+                nonzeros[:] = w[:-1] != 0
+                w[-1] = model.intercept_
+                logger.info('Operator %d decreases the cost', k)
+            else:
+                logger.info('Operator %d does not decrease the cost', k)
+            #nilc_iteration(w, lamb, X_a, X_new)
+        logger.info('%d operadores selecionados', nonzeros.sum()-1)
+        if k == max_iter-1:
+            break
+    
+    #model_cv = LogisticRegressionCV()
+            
+
+
+def nilc(training_set1, training_set2, validation, operator_generator, domain, lamb=1, max_iter=100):
     '''
     Executes the NILC algorithm. Can be done with 
     preexecuted operators or generating new ones on
     the fly. 
     '''
-    w = np.zeros(max_iter)
-    X_a = np.zeros((training_set.num_samples(domain), max_iter+1))
-    X_a[:,0] = 1
-    nonzeros = [False]* (max_iter+1)    
+    w = np.zeros(max_iter+1)
+    X_a = np.zeros((training_set2.num_samples(domain), max_iter))
+    nonzeros_plus_bias = np.zeros(max_iter+1, np.bool)
+    nonzeros_plus_bias[-1] = True
+    nonzeros = nonzeros_plus_bias[:-1]
     
-    for k, operator in enumerate(operator_generator(domain)):
+    model = LogisticRegression(penalty='l1', C=1/lamb, fit_intercept=True)
+    
+    for k, operator in enumerate(operator_generator(training_set1, domain)):
         logger.info('Iteration %d', k)
-        if X_a is None:
-            pass
-            # do first iteration
-        else:
-            X_new, _ = operator.extract_dateaset(training_set)
-            nilc_iteration(w, lamb, X_a, X_new)
         
-        if k == max_iter:
+        X, y = operator.extractor.extract_dataset(training_set2, True)
+        y = y/255
+        X_a[:, k] = operator.classifier.apply_batch(X)
+
+        if k == 0:
+            nonzeros[0] = True
+            model.fit(X_a[:, nonzeros], y)
+            w[0] = model.coef_
+            w[-1] = model.intercept_
+        else:
+            if opt_condition(X_a, y, w, lamb, k):
+                nonzeros[k] = True
+                model.fit(X_a[:, nonzeros], y)
+                w[nonzeros_plus_bias] = model.coef_
+                nonzeros[:] = w[:-1] != 0
+                w[-1] = model.intercept_
+                logger.info('Operator %d decreases the cost', k)
+            else:
+                logger.info('Operator %d does not decrease the cost', k)
+            #nilc_iteration(w, lamb, X_a, X_new)
+        logger.info('%d operadores selecionados', nonzeros.sum()-1)
+        if k == max_iter-1:
             break
         
 
