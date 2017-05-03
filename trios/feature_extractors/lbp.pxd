261a2ed96a514313b3b0f1b787a53e2e10275062
Augusto <augusto.cms7@gmail.com>
2017-05-03 20:16:24 -0300

    LBP optimized
    Signed-off-by: igor <igordsm@gmail.com>

--- trios/feature_extractors/lbp.pxd
+++ trios/feature_extractors/lbp.pxd
@@ -1,6 +1,7 @@
 from trios.WOperator cimport FeatureExtractor
+from trios.feature_extractors.raw cimport RAWFeatureExtractor
 import numpy as np
 
-cdef class LBPExtractor(FeatureExtractor):
+cdef class LBPExtractor(RAWFeatureExtractor):
    cdef int k 
 
--- trios/feature_extractors/lbp.pyx
+++ trios/feature_extractors/lbp.pyx
@@ -1,18 +1,16 @@
+cimport numpy as np
 import numpy as np
 cimport cython
+import skimage.morphology as sk
 
 from trios.WOperator import FeatureExtractor
 from trios.WOperator cimport FeatureExtractor, raw_data
 from trios.serializable import Serializable
 from trios.serializable cimport Serializable
+from trios.feature_extractors.raw cimport RAWFeatureExtractor
+import trios.shortcuts.persistence as p
 
-cdef class LBPExtractor(FeatureExtractor):
-    def __init__(self, unsigned char[:,:] window=None, **kw):
-        FeatureExtractor.__init__(self, window, **kw)
-        self.dtype = np.uint8
-
-    def __len__(self):
-        return np.greater(self.window, 0).sum()
+cdef class LBPExtractor(RAWFeatureExtractor):
 
     def calculate_lbp(self, unsigned char[:,:] img, int i, int j):
         cdef int lbp[8]
@@ -20,24 +18,24 @@ cdef class LBPExtractor(FeatureExtractor):
         W = img.shape[1]
         if i > 0 and i < H-1 and j > 0 and j < W-1:
             lbp[:] = [ int(img[i - 1, j] > img[i,j]), int(img[i - 1, j + 1] > img[i,j]), int(img[i, j + 1] > img[i,j]), int(img[i + 1, j + 1] > img[i,j]), int(img[i + 1, j] > img[i,j]), int(img[i + 1, j - 1] > img[i,j]), int(img[i, j - 1] > img[i,j]), int(img[i - 1, j - 1] > img[i,j])]
+        else: 
+            return 0
         return int("".join(str(p) for p in lbp),2)
 
     @cython.boundscheck(False)
     @cython.nonecheck(False)
-    cpdef extract(self, unsigned char[:,:] img, int i, int j, pat):
-        cdef unsigned char [:]pattern = pat
-        cdef unsigned char[:, :] win = self.window
-        cdef int hh = win.shape[0]
-        cdef int ww = win.shape[1]
-        cdef int hh2 = hh/2
-        cdef int ww2 = ww/2
-        cdef int l, m
-        cdef int k = 0
+    cpdef extract_batch(self, unsigned char[:,:] inp, idx_i, idx_j, np.ndarray X):
+        cdef unsigned char[:,:] lbp_img = np.zeros_like(inp)
+        mask = np.zeros_like(inp)
+        mask[idx_i, idx_j] = 1
+        mask = sk.dilation(mask, self.window)
+        idx_im, idx_jm = np.nonzero(mask); 
 
-        for l in range(-hh2, hh2+1):
-            for m in range(-ww2, ww2+1):
-                pat[k] = self.calculate_lbp(img, i + l, j + m)
-                k += 1
+        for l in range(idx_im.shape[0]):
+            lbp_img[idx_im[l], idx_jm[l]] = self.calculate_lbp(inp, idx_im[l], idx_jm[l])
+        p.save_image(lbp_img, "lbp.png")
+        for l in range(idx_i.shape[0]):
+            self.extract(lbp_img, idx_i[l], idx_j[l], X[l])
 
     def write_state(self, obj_dict):
         FeatureExtractor.write_state(self, obj_dict)
