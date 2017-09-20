1133952f7ad9b6cc645b2bec5fc196ac0ac7fb1f
Augusto Cesar <augusto.cms7@gmail.com>
2017-09-20 17:37:45 -0300

    __init__ added on trios.contrib.features

---
+++ trios/contrib/features/__init__.py
@@ -0,0 +1,5 @@
+from .lbp import LBPExtractor
+from .fourier import FourierExtractor
+from .moments import MomentsExtractor
+from .sobel import SobelExtractor
+from .featurecombination import FeatureCombinationExtractor
