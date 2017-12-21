ec05d811923394c11a4314c5f0e23c64d14580a1
Igor Montagner <igordsm@gmail.com>
2017-12-21 09:52:30 -0200

    Small fix to save image in using_trained_operators.py

--- docs/examples/using_trained_operator.py
+++ docs/examples/using_trained_operator.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     # The jung operator uses the input image as mask. Thus, it erases 
     # all pixels that do not belong to an s.
     result = jung_op.apply(input13, input13) 
-    
+    p.save_image(result, 'jung13-output.png')
+
     testset = trios.Imageset.read('jung-images/test.set')
     print('Error:', jung_op.eval(testset))
 
